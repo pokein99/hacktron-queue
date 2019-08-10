96f68642c1de2d23688204090e39385010b91acd
vdmehta36 <vdmehta36@gmail.com>
2019-08-10 13:03:24 +0530

    updated swagger version
    added cross origin

--- dbsQueue/src/main/java/com/dbs/hacktron/controller/QueueController.java
+++ dbsQueue/src/main/java/com/dbs/hacktron/controller/QueueController.java
@@ -6,6 +6,7 @@ import java.util.Queue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dbs.hacktron.service.QueueService;
 
+@CrossOrigin(origins = "*")
 @RestController("/dbs")
 public class QueueController {
 	
