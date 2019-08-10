ea28a91a51f1e879c783cc61b6d2fc7329c69cef
user <user@DESKTOP-THI8J67>
2019-08-09 23:51:25 -0700

    adding exception class

---
+++ dbsQueue/src/exception/java/com/dbs/hacktron/TestQueueException.java
@@ -0,0 +1,25 @@
+package hacktron.exception;
+
+import hacktron.exception.QueueException;
+
+public class TestQueueException {
+	
+	
+	static void isfull(String message, int qid) throws QueueException
+	{
+		throw new QueueException("Queue is full!",qid);
+	}
+	public static void main(String [] args)
+	{
+		String s = "testmesssage";
+		int qid = 5;
+		try{
+			isfull(s,qid);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception occurred: "+ e.getMessage());			
+		}
+	}
+
+}
